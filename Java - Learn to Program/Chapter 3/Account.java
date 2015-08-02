7c65364c9b55537a2c22a1b66400a94b801770c7
Cris Echeguren <Cris.Echeguren@gmail.com>
2015-08-02 10:58:48 -0700

    completed up to 3.5

--- Java - Learn to Program/Chapter 3/Account.java
+++ Java - Learn to Program/Chapter 3/Account.java
@@ -1,10 +1,34 @@
-//Fig 3.1 Account.java
-// Account class that contains a "name" instance variable
-// and methods to set and get its value.
+// Chapter 3 Account.java
+// Account class with a constructor.
 
 public class Account
 {
 	private String name; // instance variable
+	private double balance; // instance variable
+
+	// constructor initializes name with parameter name & balance with parameter balance
+	// ** the constructor's name is the same as the class' name **
+	public Account(String name, double balance)
+	{
+		this.name = name; // assign name to instance variable name
+
+		// validate that the balance is greater than 0.0; if not, it keeps it's default initial value of 0.0
+		if (balance > 0.0) // if balance is greater than 0.0
+			this.balance = balance; // assign it to instance variable balance
+	} // end constructor Account
+
+	// method that deposits (adds) only a valid amount to the balance
+	public void deposit(double depositAmount)
+	{
+		if (depositAmount > 0.0) // if the deposit amount is valid
+			balance += depositAmount; // add depositAmount to balance
+	} // end method deposit
+
+	// method returns the account balance
+	public double getBalance()
+	{
+		return balance;
+	}
 
 	// method to set the name of the object
 	public void setName(String name)
--- Java - Learn to Program/Chapter 3/AccountTest.java
+++ Java - Learn to Program/Chapter 3/AccountTest.java
@@ -1,4 +1,4 @@
-// Fig. 3.2 AccountTest.java
+// Chapter 3 AccountTest.java
 // Creating and manipulating an Account object.
 import java.util.Scanner;
 
@@ -9,8 +9,8 @@ public class AccountTest
 		// Create a Scanner object to obtain input from the command window.
 		Scanner keyboard = new Scanner(System.in);
 
-		// Create an Account object and assign it to myAccount.
-		Account myAccount = new Account();
+		// Constructing an account with Nothing in it.
+		Account myAccount = new Account(null, 0.0);
 
 		// display initial value of name (null)
 		System.out.printf("Initial name is : %s%n%n", myAccount.getName());
@@ -23,5 +23,15 @@ public class AccountTest
 
 		// display the name stored in object myAccount
 		System.out.printf("Name in object myAccount is:%n%s%n", myAccount.getName());
+
+		// Using the Account constructor to initialize the name instance variable at the time each Account object is created
+		// create two account objects
+		Account account1 = new Account("Jake Doe", 4.32);
+		Account account2 = new Account("Jane Green", 5323.32);
+
+		// display initial value of name for each Account
+		System.out.printf("account name is: %s%n", account1.getName());
+		System.out.printf("account name is: %s%n", account2.getName());
+		System.out.println(account2.getName() + " has " + account2.getBalance() + " dollars.");
 	}
 } // end class AccountTest
