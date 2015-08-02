e1abc4218b533362b54c04840052b5e141f07e94
Cris Echeguren <Cris.Echeguren@gmail.com>
2015-08-02 14:10:07 -0700

    started graphics case study

---
+++ Java - Learn to Program/Graphics Case Study/Dialog1.java
@@ -0,0 +1,10 @@
+import javax.swing.JOptionPane;
+
+public class Dialog1
+{
+	public static void main(String[] args)
+	{
+		// display a dialog with a message
+		JOptionPane.showMessageDialog(null, "Welcome to Java!");
+	} // end main method
+} // end class Dialog1
